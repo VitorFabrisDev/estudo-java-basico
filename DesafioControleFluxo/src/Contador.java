2e40456ee74ee0e525c7b7b840d3897378c17718
VitorFabrisDev <vitor2fabris@hotmail.com>
2023-09-19 21:29:22 -0300

    Projeto DesafioControleFluxo

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -1,5 +1,43 @@
+import java.util.Scanner;
+
 public class Contador {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner terminal = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro número da sequência: ");
+        int iParametroUm = terminal.nextInt();
+        System.out.println("Digite o segundo número da sequência: ");
+        int iParametroDois = terminal.nextInt();
+
+        try {
+
+            contar(iParametroUm, iParametroDois);
+
+        }catch(ParametrosInvalidosException e) {
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+
+        }
     }
+
+    static void contar(int iParametroUm, int iParametroDois) throws ParametrosInvalidosException{
+        //Validar se iParametroUm é MAIOR que iParametroDois e lançar exceção.
+
+        if(iParametroUm > iParametroDois) {
+            throw new ParametrosInvalidosException();
+
+        }else {
+            int iContagem = iParametroDois - iParametroUm;
+
+            for(int i = 0; i < iContagem; i++) {
+                System.out.println("Imprimindo numero: " + (i+1));
+            }
+
+
+        }
+
+
+
+    }
+
 }
